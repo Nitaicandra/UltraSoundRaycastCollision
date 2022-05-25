be9cd7ddcfc6cc5ef5fdd58667fa2f8d37e91ba4
Nitaicandra <89361982+Nitaicandra@users.noreply.github.com>
2022-05-24 22:46:24 -0400

    Add files via upload

---
+++ RayController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayController : MonoBehaviour
+{
+    public GameObject[] raycasters;
+    public CollisionReverseRaycast[] script_list;
+    public float pressure_threshold=0.03f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        raycasters = GameObject.FindGameObjectsWithTag("raycaster");
+        script_list = new CollisionReverseRaycast[raycasters.Length];
+        for(int i = 0; i< raycasters.Length;i++) {
+            script_list[i] = raycasters[i].GetComponent<CollisionReverseRaycast>();
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < script_list.Length; i++)
+        {
+            if (script_list[i].pressure>pressure_threshold)
+            {
+                Debug.Log("VEIN COLLAPSED " + "Pressure " + 
+                    script_list[i].pressure + " OBJECT " + script_list[i].gameObject);
+            }
+        }
+    }
+}
